77fdd142d0c762b09b4e19426c6330761e387208
athiru <a.thiru@gmail.com>
2017-03-21 09:56:30 -0700

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+define('DB_NAME_DEMO', 'dr_appt_db');
+
+/** MySQL database username */
+define('DB_USER', 'xxx');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'xxx');
+
+/** MySQL hostname */
+define('DB_HOST', 'eduprez5db.cb8l5ruzu25emg.ap-southeast-1.rds.amazonaws.com');
+
+/** MySQL port */
+define('DB_PORT', '3306');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
